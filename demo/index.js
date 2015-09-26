52badf56b37e5686f2385b2a30fd77e2f9e966a7
Bhargav Anand <rjn143@gmail.com>
2015-09-26 14:15:39 -0700

    Refactor: simplify main function

--- demo/index.js
+++ demo/index.js
@@ -5,13 +5,9 @@ main();
 function main() {
 	let app;
 
-	if (process.env.NODE_ENV === 'development') {
-		app = document.createElement('div');
-		app.setAttribute('id', 'demo');
-		document.body.appendChild(app);
-	} else {
-		app = document.getElementById('demo');
-	}
+	app = document.createElement('div');
+	app.setAttribute('id', 'demo');
+	document.body.appendChild(app);
 
 	React.render(<Demo />, app);
 }
