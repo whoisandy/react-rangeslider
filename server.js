367e45ed3a0be9c6b98a3dd064fed056d72c893f
Bhargav Anand <rjn143@gmail.com>
2015-09-25 20:55:48 -0700

    Chore: move server to root

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-'use strict';
-
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('../webpack.config');
+var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
 	contentBase: __dirname,
 	hot: true,
+	inline: true,
 	stats: {
 		chunkModules: false,
 		colors: true,
