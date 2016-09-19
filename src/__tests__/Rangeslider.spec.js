5fea7ca5bee04d4da30ac2a899a4d1e1f4c43511
Bhargav Anand <rjn143@gmail.com>
2016-09-18 20:15:10 -0700

    Add basic tests for slider

---
+++ src/__tests__/Rangeslider.spec.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import Slider from '../'
+
+describe('Rangeslider specs', () => {
+  it('should render properly', () => {
+    const slider = shallow(<Slider />)
+    expect(slider.hasClass('rangeslider')).toBeTruthy()
+    expect(slider.children().length).toEqual(2)
+    expect(slider.find('.rangeslider__fill').length).toEqual(1)
+    expect(slider.find('.rangeslider__handle').length).toEqual(1)
+  })
+
+  it('should have default props', () => {
+    const slider = mount(<Slider />)
+    expect(slider.prop('min')).toEqual(0)
+    expect(slider.prop('max')).toEqual(100)
+    expect(slider.prop('step')).toEqual(1)
+    expect(slider.prop('value')).toEqual(0)
+    expect(slider.prop('orientation')).toEqual('horizontal')
+  })
+})
--- src/__tests__/Sanity.spec.js
+++ src/__tests__/Sanity.spec.js
@@ -1,4 +1,4 @@
-describe('Sanity test', () => {
+describe('Sanity Specs', () => {
   it('should evaluate true to be truthy', () => {
     expect(true).toBeTruthy()
   })
