96b6cac595a3e72fa335c35d8fa1b935ce89417c
Bhargav Anand <rjn143@gmail.com>
2016-09-18 19:39:07 -0700

    Integrate jest & enzyme; Add boilerplate test

---
+++ src/__tests__/Sanity.spec.js
@@ -0,0 +1,5 @@
+describe('Sanity test', () => {
+  it('should evaluate true to be truthy', () => {
+    expect(true).toBeTruthy()
+  })
+})
--- tests/SanitySpec.js
+++
@@ -1,9 +0,0 @@
-describe('Sanity test', () => {
-  it('should evaluate true to be ok', () => {
-    expect(true).to.be.ok
-  })
-
-  it('should evaluate true to be true', () => {
-    expect(true).to.be.true
-  })
-})
