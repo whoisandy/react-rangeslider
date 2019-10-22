5fbe5303b70b155259120d6c9c64692c6ac23bbf
Joel Courtney <joel@aceteknologi.com>
2019-10-22 17:55:34 +1100

    Support element for handleLabel

--- src/Rangeslider.js
+++ src/Rangeslider.js
@@ -36,7 +36,10 @@ class Slider extends Component {
     tooltip: PropTypes.bool,
     reverse: PropTypes.bool,
     labels: PropTypes.object,
-    handleLabel: PropTypes.string,
+    handleLabel: PropTypes.oneOfType[
+      PropTypes.string,
+      PropTypes.element
+    ],
     format: PropTypes.func,
     onChangeStart: PropTypes.func,
     onChange: PropTypes.func,
